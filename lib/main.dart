4872ae108ee6d25813621323c1325fe5791077af
Alvaro-Antonio <alvaro.antonio@dce.ufpb.br>
2020-04-28 19:52:35 -0300

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Lista de Tarefas",
+    home: _Home(),
+
+  ));
+}
+
+class _Home extends StatefulWidget {
+  @override
+  __HomeState createState() => __HomeState();
+}
+
+class __HomeState extends State<_Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
