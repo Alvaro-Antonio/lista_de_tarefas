780495740e3e5be93245ecdbe56a7298fc2357ce
Alvaro-Antonio <alvaro.antonio@dce.ufpb.br>
2020-04-29 23:12:38 -0300

    adicionado snackbar com opção desfazer delete

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class _Home extends StatefulWidget {
 class __HomeState extends State<_Home> {
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -114,10 +117,36 @@ class __HomeState extends State<_Home> {
           child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
         ),
       ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: (){
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+
+                }),
+            duration: Duration(seconds: 2),
+
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+
+        });
+      },
     );
   }
 
-  /**/
+
   /* Funções de leitura de arquivos*/
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
